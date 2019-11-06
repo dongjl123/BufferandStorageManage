85297df87f1ca2e667763fb20fdc2cd72e9b3d47
wxy1228 <403812699@qq.com>
2019-11-06 21:48:36 +0800

    hash done

--- readme.md
+++ readme.md
@@ -8,9 +8,19 @@ $ make
 $ cd bin
 $ ./main
 ```
+windows命令行下的运行方法
+```shell
+mkdir build
+cd build
+cmake -G"MinGW Makefiles" ../
+mingw32-make
+cd bin
+main.exe
+```
 
+---
 ### 数据结构
-- 缓冲区LRU链表：用于buffer存放数据，LRU的数据结构
+1. 缓冲区LRU链表：用于buffer存放数据，LRU的数据结构
 ```c
 struct LRU_element
 {
@@ -22,19 +32,45 @@ struct LRU_element
     LRU_element *next;     
 };
 ```
-- 缓冲区控制块BCB
-- 哈希桶：用于判断是否命中(是否在LRU链表中)
-- 文件索引块：用于查找文件存放的位置
-- 文件数据块：用于存储数据
+2. 缓冲区控制块BCB
+```c
+struct BCB
+{
+    bool isHead;
+    bool isTrail;
+    int frameID;
+    int pageID;
+    bool isWrite;
+    BCB *front;
+    BCB *next;
+};
+```
+3. 哈希桶：用于判断是否命中(是否在LRU链表中)
+
+`BCB *HashTable[buffersize]`
+
+基本思想是通过`hash(pageID) = pageID % buffersize`找到在哈希桶的哪一个位置，然后采用链表的方法存入。
+
+4. 文件索引块：用于查找文件存放的位置
+5. 文件数据块：用于存储数据
+6. FrameSize = 4096，DefBufferSize = 1024
 
 ---
-### 函数功能
+### 主要功能
 #### LRU缓冲区
-1. FrameSize = 4096，DefBufferSize = 1024
-2. `init_LRU()`和用于LRU链表的初始化
-3. `LRU_element* return_head()`和`LRU_element* return_tail()`用于返回链表的头节点和尾节点
-4. `void insert_node(LRU_element *new_node)`用于向链表尾部添加新的节点
-5. `void drop_head()`用于drop链表中的第一个节点
-6. `void adjust_LRU(LRU_element *node)`用于LRU算法中节点的位置变更(把该节点放到链表的末尾) 
-7. `int return_len()`用于返回LRU链表的长度
-8. `bool FULL_LRU()`用于判断LRU链表是否已满
+
+1. `init_LRU()`用于LRU链表的初始化
+2. `LRU_element* return_head()`和`LRU_element* return_tail()`用于返回链表的头节点和尾节点
+3. `void insert_node(LRU_element *new_node)`用于向链表尾部添加新的节点
+4. `void drop_head()`用于drop链表中的第一个节点
+5. `void adjust_LRU(LRU_element *node)`用于LRU算法中节点的位置变更(把该节点放到链表的末尾) 
+6. `int return_len()`用于返回LRU链表的长度
+7. `bool FULL_LRU()`用于判断LRU链表是否已满
+
+#### Hash哈希桶
+
+1. `init_Hash()`用于Hash的初始化
+2. `int hash`用于计算hash值，为`pageID % buffersize`
+3. `insert_BCB`和`drop_BCB`用于插入和删除节点
+4. `has_page`用于判断是否在表中，为bool型
+5. `find_BCB`用于在`has_page`为真的时候返回该pageID所对应的节点
---
+++ src/buffer/Hash.cpp
@@ -0,0 +1,94 @@
+#include "Hash.hpp"
+
+Hash::Hash(void)
+{
+    cout<<"Hash has been created."<<endl;
+}
+
+Hash::~Hash()
+{
+    cout<<"Hash has been dropped."<<endl;
+}
+
+/*计算hash的值*/
+int Hash::hash(int pageID)
+{
+    return pageID%BufSize;
+}
+
+/*Hash的初始化*/
+void Hash::init_Hash()
+{
+    for(int i=0; i<BufSize; i++)
+    {
+        BCB *head = new BCB;
+        BCB *trail = new BCB;
+
+        head->isHead = true;
+        head->isTrail = false;
+        head->front = nullptr;
+        head->next = trail;
+
+        trail->isHead = false;
+        trail->isTrail = true;
+        trail->front = head;
+        trail->next = nullptr;
+
+        HashTable[i] = head;
+    }
+}
+
+/*加入哈希桶*/
+void Hash::insert_BCB(int frame_ID, int page_ID, bool is_Write)
+{
+    BCB *new_BCB = new BCB;
+    new_BCB->frameID = frame_ID;
+    new_BCB->pageID = page_ID;
+    new_BCB->isWrite = is_Write;
+    new_BCB->isHead = false;
+    new_BCB->isTrail = false;
+
+    int BCB_hash = hash(page_ID);
+    new_BCB->front = HashTable[BCB_hash];
+    new_BCB->next = HashTable[BCB_hash]->next;
+    HashTable[BCB_hash]->next->front = new_BCB;
+    HashTable[BCB_hash]->next = new_BCB;
+}
+
+/*删除哈希桶中的一个节点*/
+void Hash::drop_BCB(BCB *victim_BCB)
+{
+    victim_BCB->front->next = victim_BCB->next;
+    victim_BCB->next->front = victim_BCB->front;
+}
+
+/*判断该page_ID是否在已经buffer中*/
+bool Hash::has_page(int page_ID)
+{
+    int BCB_hash = hash(page_ID);
+    BCB *node = HashTable[BCB_hash];
+    while(!node->next->isTrail)
+    {
+        if(node->next->pageID==page_ID)
+        {
+            return true;
+        }
+        node = node->next;
+    }
+    return false;
+}
+
+/*找到该page_ID所对应的哈希桶中的节点*/
+BCB *Hash::find_BCB(int page_ID)
+{
+    int BCB_hash = hash(page_ID);
+    BCB *node = HashTable[BCB_hash];
+    while(!node->next->isTrail)
+    {
+        if(node->next->pageID==page_ID)
+        {
+            return node->next;
+        }
+        node = node->next;
+    }
+}
